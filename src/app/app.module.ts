c8aea501e2828715b7890f6aa3a13d414bb3bf16
Guy Leifer <guyl@moveo.co.il>
2021-10-19 17:08:05 +0300

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardComponent } from './components/card/card.component';
+import { WeeklyChefComponent } from './components/weekly-chef/weekly-chef.component';
+import { AboutComponent } from './common/about/about.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    CardsComponent,
+    CardComponent,
+    WeeklyChefComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
